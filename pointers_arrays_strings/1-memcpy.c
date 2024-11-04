0e812288be01dc758e397df7455a891da5073554
adoniskoco <10028@holbertonstudents.com>
2024-11-04 10:45:29 +0000

    added new file

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memcpy - fills memory with byte
+* @dest: points to the memory
+* @src: points to source
+* @n: number of times to insert the char
+* Return: (dest)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	 unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
