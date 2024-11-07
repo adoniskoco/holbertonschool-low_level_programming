cd84353740dfa47d1a9d50c8db79e23b8ebdb852
adoniskoco <10028@holbertonstudents.com>
2024-11-07 14:04:13 +0000

    added file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - entry point of the program
+* @argc: count of arguments
+* @argv: list of arguments
+* Return: 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
