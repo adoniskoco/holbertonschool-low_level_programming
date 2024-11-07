dd972e022761a59cb3d0cf8e23e58a3125b3e71e
adoniskoco <10028@holbertonstudents.com>
2024-11-07 14:12:10 +0000

    added file

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point of the program
+* @argc: count of arguments
+* @argv: list of arguments
+* Return: 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
