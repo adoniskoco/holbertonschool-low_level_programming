99aa50aa503a1d05e6a8668764ef6993df6ed0df
adoniskoco <10028@holbertonstudents.com>
2024-11-12 09:42:30 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,41 +1,49 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
- * str_concat - function to concate 2 arry
+ * str_concat - function to concate 2 arrays
  * @s1: first string
  * @s2: second string
  * Return: null or a pointer to s
  */
+
 char *str_concat(char *s1, char *s2)
 {
-int i, j, size;
-char *result;
+	char *result;
+	unsigned int i, j;
+	int size;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
-i = 0;
+	i = 0;
 	while (s1[i] != '\0')
 		i++;
 
 	j = 0;
 	while (s2[j] != '\0')
 		j++;
-	size = i + j;
+
+	size = i + j + 1;
 
 	result = malloc(size * sizeof(char));
 	if (result == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; s1[i] != '\0'; i++)
-		result[i] = s1[i];
-
+	result[i] = s1[i];
 	for (j = 0; s2[j] != '\0'; j++)
-		result[i + j] = s2[j];
+	result[i + j] = s2[j];
 
 	return (result);
 
