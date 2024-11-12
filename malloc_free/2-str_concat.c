3150412f5cb4ea3d35d7a32400ecc0ddeb7df3dd
adoniskoco <10028@holbertonstudents.com>
2024-11-12 09:32:46 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,7 @@
+#include "main.h"
+#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * str_concat - function to concate 2 arry
  * @s1: first string
@@ -8,23 +10,33 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i;
-	char *s;
+int i, j, size;
+char *result;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	s = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
-	if (s == NULL)
+i = 0;
+	while (s1[i] != '\0')
+		i++;
+
+	j = 0;
+	while (s2[j] != '\0')
+		j++;
+	size = i + j;
+
+	result = malloc(size * sizeof(char));
+	if (result == NULL)
 		return (NULL);
 
-	for (i = 0; i < strlen(s1); i++)
-		s[i] = s1[i];
+	for (i = 0; s1[i] != '\0'; i++)
+		result[i] = s1[i];
 
-	for (i = 0; i < strlen(s2); i++)
-		s[strlen(s1) + i] = s2[i];
-	s[strlen(s1) + i] = '\0';
-	return (s);
+	for (j = 0; s2[j] != '\0'; j++)
+		result[i + j] = s2[j];
+
+	return (result);
+
 }
