d00d56a8fbf5c6dfc88132d669d73f11ca484b3f
adoniskoco <10028@holbertonstudents.com>
2024-11-20 11:58:55 +0000

    added new file

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(args, char *);
+		str = va_arg(args, char*);
 		if (str == NULL)
 		{
 			printf("(nil)");
@@ -30,7 +30,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		if (i < n - 1 && separator != NULL)
 		{
-			printf(", ");
+			printf("%s", separator);
 		}
 	}
 	va_end(args);
