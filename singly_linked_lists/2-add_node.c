50a997151f58d03e1bf3673c23212f8ff1e61593
adoniskoco <10028@holbertonstudents.com>
2024-12-03 09:43:45 +0000

    Add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,23 +1,53 @@
-#include"lists.h"
-#include<string.h>
-#include<stdlib.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
- * add_node -adds a new node
- * @head: -points to the first node
- * @str: -points to string input
- * Return: (new)
+ * string_length - Calculates the length of a string manually.
+ * @str: The string whose length to calculate.
+ *
+ * Return: The length of the string.
  */
+unsigned int string_length(const char *str)
+{
+	unsigned int len = 0;
 
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Double pointer to the head of the list.
+ * @str: String to be duplicated and added to the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new = malloc(sizeof(list_t));
+	list_t *new_node;
+	char *dup_str;
 
-	if (new == NULL)
+	if (str == NULL)
 		return (NULL);
-	new->str = strdup(str);
-	new->next = *head;
-	*head = new;
 
-	return (new);
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(dup_str);
+		return (NULL);
+	}
+
+	new_node->str = dup_str;
+	new_node->len = string_length(dup_str);
+
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
